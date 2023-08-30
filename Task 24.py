dcc76e75574812ebf9a2c80e8992e15e9dceb45a
chistyakovdennis <chistyakov.d@gmail.com>
2023-08-30 16:39:36 +0300

    Решение задачи 22

--- Task 24.py
+++ Task 24.py
@@ -8,15 +8,12 @@
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль,
 # находясь перед некоторым кустом заданной во входном файле грядки.
 
-
-n = int(input())
+n_bushes = int(input('Введите количество кустов черники: '))
 arr = list()
-for i in range(n):
-    x = int(input())
-    arr.append(x)
-
+for i in range(n_bushes):
+    a =int(input('Введите количество ягод на кусте: '))
+    arr.append(a)
 arr_count = list()
-for i in range(len(arr) - 1):
-    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
-arr_count.append(arr[-2] + arr[-1] + arr[0])
+for i in range(len(arr)):
+       arr_count.append(arr[i-2] + arr[i-1] + arr[i])
 print(max(arr_count))
